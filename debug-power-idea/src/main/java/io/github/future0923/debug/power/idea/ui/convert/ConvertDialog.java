de8e4bf38264eefd1be2e4dc0d8733aef401aa17
future0923 <future94@qq.com>
2024-05-13 13:50:03 +0800

    导入逻辑与attach状态检测优化

--- debug-power-idea/src/main/java/io/github/future0923/debug/power/idea/ui/convert/ConvertDialog.java
+++ debug-power-idea/src/main/java/io/github/future0923/debug/power/idea/ui/convert/ConvertDialog.java
@@ -1,9 +1,16 @@
 package io.github.future0923.debug.power.idea.ui.convert;
 
+import com.google.gson.JsonObject;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.psi.PsiClassType;
+import com.intellij.psi.PsiParameter;
+import com.intellij.psi.PsiParameterList;
+import com.intellij.psi.PsiTypes;
+import io.github.future0923.debug.power.common.enums.RunContentType;
 import io.github.future0923.debug.power.common.utils.DebugPowerJsonUtils;
 import io.github.future0923.debug.power.idea.ui.JsonEditor;
+import io.github.future0923.debug.power.idea.utils.DebugPowerJsonElementUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -45,8 +52,22 @@ public class ConvertDialog extends DialogWrapper {
         if (ConvertType.IMPORT.equals(convertType)) {
             String text = convertPanel.getEditorTextField().getText();
             if (convertPanel.getJson().isSelected()) {
+                PsiParameterList psiParameterList = jsonEditor.getPsiParameterList();
+                if (psiParameterList != null && psiParameterList.getParametersCount() == 1) {
+                    PsiParameter parameter = psiParameterList.getParameter(0);
+                    if (parameter != null && parameter.getType() instanceof PsiClassType) {
+                        JsonObject jsonObject = new JsonObject();
+                        JsonObject argContent = new JsonObject();
+                        argContent.addProperty("type", RunContentType.JSON_ENTITY.getType());
+                        argContent.add("content", DebugPowerJsonUtils.getInstance().fromJson(text, JsonObject.class));
+                        jsonObject.add(parameter.getName(), argContent);
+                        jsonEditor.setText(DebugPowerJsonUtils.getInstancePretty().toJson(jsonObject));
+                        super.doOKAction();
+                        return;
+                    }
+                }
                 jsonEditor.setText(DebugPowerJsonUtils.jsonConvertDebugPowerJson(text));
-            } else if (convertPanel.getQuery().isSelected()){
+            } else if (convertPanel.getQuery().isSelected()) {
                 jsonEditor.setText(DebugPowerJsonUtils.queryConvertDebugPowerJson(text));
             }
         } else if (ConvertType.EXPORT.equals(convertType)) {
