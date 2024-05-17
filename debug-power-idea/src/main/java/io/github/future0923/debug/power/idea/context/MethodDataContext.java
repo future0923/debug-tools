fecd31673cc3b7e11e69b08fc1b450c50ad78f99
future0923 <future94@qq.com>
2024-05-17 13:19:29 +0800

    增加执行上一次请求按钮

--- debug-power-idea/src/main/java/io/github/future0923/debug/power/idea/context/MethodDataContext.java
+++ debug-power-idea/src/main/java/io/github/future0923/debug/power/idea/context/MethodDataContext.java
@@ -6,6 +6,10 @@ import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiParameterList;
 import com.intellij.psi.impl.java.stubs.impl.PsiParameterListStubImpl;
 import com.intellij.psi.impl.source.PsiParameterListImpl;
+import io.github.future0923.debug.power.idea.model.ParamCache;
+import io.github.future0923.debug.power.idea.setting.DebugPowerSettingState;
+import io.github.future0923.debug.power.idea.utils.DebugPowerActionUtil;
+import io.github.future0923.debug.power.idea.utils.DebugPowerIdeaClassUtil;
 import lombok.Getter;
 
 import java.util.Objects;
@@ -33,17 +37,23 @@ public class MethodDataContext {
     @Getter
     private final PsiMethod psiMethod;
 
+    @Getter
+    private final String cacheKey;
+
     /**
      * 缓存数据
      */
     public final String cacheContent;
 
-    public MethodDataContext(ClassDataContext classDataContext, String qualifiedMethodName, PsiMethod psiMethod, String cacheContent, Project project) {
+    public MethodDataContext(ClassDataContext classDataContext, PsiMethod psiMethod, Project project) {
         this.project = project;
-        this.cacheContent = cacheContent;
+        this.cacheKey = DebugPowerActionUtil.genCacheKey(classDataContext.getPsiClass(), psiMethod);
+        DebugPowerSettingState settingState = DebugPowerSettingState.getInstance(project);
+        ParamCache cache = settingState.getCache(cacheKey);
+        this.cacheContent = cache.getContent();
         this.psiMethod = psiMethod;
         this.classDataContext = classDataContext;
-        this.qualifiedMethodName = qualifiedMethodName;
+        this.qualifiedMethodName = DebugPowerIdeaClassUtil.getMethodQualifiedName(psiMethod);
     }
 
     public PsiClass getPsiClass() {
--- debug-power-idea/src/main/java/io/github/future0923/debug/power/idea/ui/main/MainDialog.java
+++ debug-power-idea/src/main/java/io/github/future0923/debug/power/idea/ui/main/MainDialog.java
@@ -1,32 +1,50 @@
 package io.github.future0923.debug.power.idea.ui.main;
 
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.text.StringUtil;
+import io.github.future0923.debug.power.common.dto.RunContentDTO;
+import io.github.future0923.debug.power.common.utils.DebugPowerFileUtils;
 import io.github.future0923.debug.power.common.utils.DebugPowerJsonUtils;
+import io.github.future0923.debug.power.idea.action.QuickDebugEditorPopupMenuAction;
 import io.github.future0923.debug.power.idea.context.MethodDataContext;
+import io.github.future0923.debug.power.idea.model.ParamCache;
+import io.github.future0923.debug.power.idea.model.ServerDisplayValue;
+import io.github.future0923.debug.power.idea.setting.DebugPowerSettingState;
 import io.github.future0923.debug.power.idea.ui.JsonEditor;
-import lombok.Setter;
+import io.github.future0923.debug.power.idea.utils.DebugPowerActionUtil;
+import io.github.future0923.debug.power.idea.utils.DebugPowerAttachUtils;
+import io.github.future0923.debug.power.idea.utils.DebugPowerNotifierUtil;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.util.Objects;
-import java.util.function.BiConsumer;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author future0923
  */
 public class MainDialog extends DialogWrapper {
 
+    private static final Logger log = Logger.getInstance(MainDialog.class);
+
     private final Project project;
 
     private final MethodDataContext methodDataContext;
 
     private MainPanel mainPanel;
-
-    @Setter
-    private BiConsumer<String, String> okAction;
 
     public MainDialog(MethodDataContext methodDataContext, @Nullable Project project) {
         super(project, true, IdeModalityType.MODELESS);
@@ -46,11 +64,54 @@ public class MainDialog extends DialogWrapper {
 
     @Override
     protected void doOKAction() {
-        if (Objects.nonNull(okAction)) {
-            JsonEditor editor = mainPanel.getEditor();
-            String auth = mainPanel.getAuthField().getText();
-            okAction.accept(auth, DebugPowerJsonUtils.compress(editor.getText()));
+        JsonEditor editor = mainPanel.getEditor();
+        String auth = mainPanel.getAuthField().getText();
+        String text = DebugPowerJsonUtils.compress(editor.getText());
+        DebugPowerSettingState settingState = DebugPowerSettingState.getInstance(project);
+        if (StringUtils.isBlank(settingState.getAgentPath())) {
+            InputStream inputStream = QuickDebugEditorPopupMenuAction.class.getResourceAsStream("/lib/debug-power-agent-jar-with-dependencies.jar");
+            if (inputStream == null) {
+                DebugPowerNotifierUtil.notifyError(project, "读取代理Jar失败");
+                return;
+            }
+            try {
+                settingState.setAgentPath(DebugPowerFileUtils.getTmpLibFile(inputStream, "agent", ".jar"));
+            } catch (IOException ex) {
+                log.error("读取代理Jar失败", ex);
+                DebugPowerNotifierUtil.notifyError(project, "读取代理Jar失败");
+                return;
+            }
         }
+        ParamCache paramCacheDto = new ParamCache(text);
+        settingState.putCache(methodDataContext.getCacheKey(), paramCacheDto);
+        Map<String, String> headers = null;
+        if (StringUtil.isNotEmpty(auth)) {
+            headers = new HashMap<>();
+            headers.put("Authorization", auth);
+            settingState.putHeader("Authorization", auth);
+        }
+        Map<String, RunContentDTO> contentMap = DebugPowerJsonUtils.toRunContentDTOMap(text);
+        String jsonDtoStr = DebugPowerJsonUtils.toDebugPowerJson(methodDataContext.getPsiClass().getQualifiedName(), methodDataContext.getPsiMethod().getName(), DebugPowerActionUtil.toParamTypeNameList(methodDataContext.getPsiMethod().getParameterList()), contentMap, headers);
+        ServerDisplayValue attach = settingState.getAttach();
+        if (attach == null || StringUtil.isEmpty(attach.getKey())) {
+            Messages.showErrorDialog("Run attach first", "执行失败");
+            return;
+        }
+        String agentParam;
+        try {
+            String pathname = project.getBasePath() + "/.idea/DebugPower/agent.json";
+            File file = new File(pathname);
+            if (!file.exists()) {
+                FileUtils.touch(file);
+            }
+            FileUtil.writeToFile(file, jsonDtoStr);
+            agentParam = "file://" + URLEncoder.encode(pathname, StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            log.error("参数写入json文件失败", ex);
+            DebugPowerNotifierUtil.notifyError(project, "参数写入json文件失败");
+            return;
+        }
+        DebugPowerAttachUtils.attach(project, attach.getKey(), settingState.getAgentPath(), agentParam);
         super.doOKAction();
     }
 
