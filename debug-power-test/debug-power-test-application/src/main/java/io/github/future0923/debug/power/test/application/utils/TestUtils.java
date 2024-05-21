b43deefaa99e1c17c39af87c130e722a6f234808
future0923 <future94@qq.com>
2024-05-21 11:25:10 +0800

    支持private方法调用

---
+++ debug-power-test/debug-power-test-application/src/main/java/io/github/future0923/debug/power/test/application/utils/TestUtils.java
@@ -0,0 +1,15 @@
+package io.github.future0923.debug.power.test.application.utils;
+
+/**
+ * @author future0923
+ */
+public class TestUtils {
+
+    public static void test() {
+        System.out.println("testStatic");
+    }
+
+    private static void testPrivate() {
+        System.out.println("testPrivateStatic");
+    }
+}
