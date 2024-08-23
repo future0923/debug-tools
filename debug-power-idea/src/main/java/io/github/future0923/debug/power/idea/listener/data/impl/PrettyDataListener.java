b2c26f93a20e8c381e578e6f6b9fc26ad06f83af
future0923 <future94@qq.com>
2024-08-23 14:56:15 +0800

    参数传递窗口优化

--- debug-power-idea/src/main/java/io/github/future0923/debug/power/idea/listener/data/impl/PrettyDataListener.java
+++ debug-power-idea/src/main/java/io/github/future0923/debug/power/idea/listener/data/impl/PrettyDataListener.java
@@ -2,16 +2,16 @@ package io.github.future0923.debug.power.idea.listener.data.impl;
 
 import io.github.future0923.debug.power.idea.listener.data.BaseDataListener;
 import io.github.future0923.debug.power.idea.listener.data.event.PrettyDataEvent;
-import io.github.future0923.debug.power.idea.ui.JsonEditor;
+import io.github.future0923.debug.power.idea.ui.main.MainJsonEditor;
 
 /**
  * @author future0923
  */
 public class PrettyDataListener extends BaseDataListener<PrettyDataEvent> {
 
-    private final JsonEditor jsonEditor;
+    private final MainJsonEditor jsonEditor;
 
-    public PrettyDataListener(JsonEditor jsonEditor) {
+    public PrettyDataListener(MainJsonEditor jsonEditor) {
         this.jsonEditor = jsonEditor;
     }
 
--- debug-power-idea/src/main/java/io/github/future0923/debug/power/idea/ui/editor/JsonEditor.java
+++ debug-power-idea/src/main/java/io/github/future0923/debug/power/idea/ui/editor/JsonEditor.java
@@ -4,11 +4,12 @@ import com.intellij.json.JsonFileType;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author future0923
  */
-public class UpgradeJsonEditor extends BaseEditor {
+public class JsonEditor extends BaseEditor {
 
     public static final String FILE_NAME = "DebugPowerEditFile.json";
 
@@ -17,16 +18,21 @@ public class UpgradeJsonEditor extends BaseEditor {
      */
     public static final FileType JSON_FILE_TYPE = JsonFileType.INSTANCE;
 
-    public UpgradeJsonEditor(@NotNull Project project) {
+    public JsonEditor(@NotNull Project project) {
         this(project, null);
     }
 
-    public UpgradeJsonEditor(Project project, String text) {
+    public JsonEditor(Project project, String text) {
         super(project, JSON_FILE_TYPE, text);
     }
 
     @Override
     protected String fileName() {
         return FILE_NAME;
+    }
+
+    @Override
+    public void setText(@Nullable String text) {
+        setText(text, JSON_FILE_TYPE);
     }
 }
