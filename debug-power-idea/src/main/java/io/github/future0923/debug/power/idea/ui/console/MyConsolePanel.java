a5bf0eb6672a63fed80cddc1f7eafbf3d52090ac
future0923 <future94@qq.com>
2024-09-04 15:53:30 +0800

    修复在2024.1及以下创建ConsoleViewAction失败的问题

--- debug-power-idea/src/main/java/io/github/future0923/debug/power/idea/ui/console/MyConsolePanel.java
+++ debug-power-idea/src/main/java/io/github/future0923/debug/power/idea/ui/console/MyConsolePanel.java
@@ -1,15 +1,12 @@
 package io.github.future0923.debug.power.idea.ui.console;
 
-import com.intellij.execution.actions.ClearConsoleAction;
 import com.intellij.execution.filters.TextConsoleBuilderFactory;
 import com.intellij.execution.ui.ConsoleView;
 import com.intellij.execution.ui.ConsoleViewContentType;
-import com.intellij.openapi.actionSystem.ActionGroup;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.ActionToolbar;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.project.Project;
-import com.intellij.ui.components.JBPanel;
 import io.github.future0923.debug.power.idea.constant.IdeaPluginProjectConstants;
 import org.jetbrains.annotations.NotNull;
 
@@ -30,19 +27,21 @@ public class MyConsolePanel extends JPanel {
     public MyConsolePanel(ConsoleView consoleView, DefaultActionGroup toolbarActions) {
         super(new BorderLayout());
         this.consoleView = consoleView;
+        // 先调用getComponent方法确保组建被正确的初始化（editor对象），这样调用createConsoleActions方法才不会报错
+        JComponent component = consoleView.getComponent();
         // 创建工具栏面板
         JPanel toolbarPanel = new JPanel(new BorderLayout());
         if (toolbarActions == null) {
             toolbarActions = new DefaultActionGroup();
+            // createConsoleActions方法必须保证有editor对象
             toolbarActions.addAll(consoleView.createConsoleActions());
         }
         ActionToolbar actionToolbar = ActionManager.getInstance().createActionToolbar(IdeaPluginProjectConstants.ROOT_TYPE_ID, toolbarActions, false);
         actionToolbar.setTargetComponent(this);
         toolbarPanel.add(actionToolbar.getComponent());
-
         // 添加组件到面板
         add(toolbarPanel, BorderLayout.WEST);
-        add(consoleView.getComponent(), BorderLayout.CENTER);
+        add(component, BorderLayout.CENTER);
     }
 
     public void print(@NotNull String text) {
