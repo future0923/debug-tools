12a9c488c1eec35e87e043e6e853f26bdc253368
future0923 <future94@qq.com>
2025-03-11 15:44:33 +0800

    修改shell编译cpp文件

--- debug-tools-vm/shell/compile-window.bat
+++ debug-tools-vm/shell/compile-window.bat
@@ -1,3 +1,7 @@
 @echo off
 set COMPILE_JAVA_HOME=%JAVA_HOME%
-g++ ../src/main/native/src/io_github_future0923_debug_tools_vm_VmTool.cpp -shared -o ../../debug-tools-server/src/main/resources/lib/libJniLibrary-x64.dll -I$COMPILE_JAVA_HOME/include -I$COMPILE_JAVA_HOME/include/win64 -static
+if "%COMPILE_JAVA_HOME%"=="" (
+    echo ERROR: _JAVA_HOME is not set. Exiting...
+    exit /b 1
+)
+x86_64-w64-mingw32-g++ ..\src\main\native\src\io_github_future0923_debug_tools_vm_VmTool.cpp -shared -o ..\..\debug-tools-server\src\main\resources\lib\libJniLibrary-x64.dll -I../src/main/native/src/ -I%COMPILE_JAVA_HOME%\include -I%COMPILE_JAVA_HOME%\include\win32 -static
