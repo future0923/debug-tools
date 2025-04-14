f914afbc3dc58070ad95cd550f37e0ca8902af72
future0923 <future94@qq.com>
2025-04-14 09:23:12 +0800

    v4.0.0-SNAPSHOT

--- debug-tools-base/src/main/java/io/github/future0923/debug/tools/base/constants/ProjectConstants.java
+++ debug-tools-base/src/main/java/io/github/future0923/debug/tools/base/constants/ProjectConstants.java
@@ -9,7 +9,7 @@ public interface ProjectConstants {
 
     String NAME = "DebugTools";
 
-    String VERSION = "3.4.3";
+    String VERSION = "4.0.0-SNAPSHOT";
 
     String SERVER_CORE_JAR_PATH = "lib/debug-tools-server.jar";
 
