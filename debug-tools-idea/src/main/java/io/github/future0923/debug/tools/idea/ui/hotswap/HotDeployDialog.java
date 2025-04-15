64e8de96fcc25b5a805c07fb060579ccf0343cf1
future0923 <future94@qq.com>
2025-04-15 09:38:26 +0800

    修改本项目文件提示判断

--- debug-tools-idea/src/main/java/io/github/future0923/debug/tools/idea/ui/hotswap/HotDeployDialog.java
+++ debug-tools-idea/src/main/java/io/github/future0923/debug/tools/idea/ui/hotswap/HotDeployDialog.java
@@ -121,7 +121,7 @@ public class HotDeployDialog extends DialogWrapper {
         jbList.setCellRenderer(new ColoredListCellRenderer<String>() {
             @Override
             protected void customizeCellRenderer(@NotNull JList<? extends String> list, String value, int index, boolean selected, boolean hasFocus) {
-                if (value.startsWith("src/main")) {
+                if (!value.startsWith("/")) {
                     append(value, new SimpleTextAttributes(SimpleTextAttributes.STYLE_PLAIN, JBColor.BLUE));
                 } else {
                     append(value);
