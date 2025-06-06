00fe6b6da382d80954230af1aaba9dd41e32e23f
future0923 <future94@qq.com>
2025-06-06 10:03:39 +0800

    适配Solon

--- debug-tools-hotswap/debug-tools-hotswap-plugin/debug-tools-hotswap-solon-plugin/src/main/java/io/github/future0923/debug/tools/hotswap/core/plugin/solon/transformer/SolonBeanClassFileTransformer.java
+++ debug-tools-hotswap/debug-tools-hotswap-plugin/debug-tools-hotswap-solon-plugin/src/main/java/io/github/future0923/debug/tools/hotswap/core/plugin/solon/transformer/SolonBeanClassFileTransformer.java
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.github.future0923.debug.tools.hotswap.core.plugin.spring.transformer;
-
+package io.github.future0923.debug.tools.hotswap.core.plugin.solon.transformer;
 
 import io.github.future0923.debug.tools.base.logging.Logger;
 import io.github.future0923.debug.tools.hotswap.core.command.Scheduler;
-import io.github.future0923.debug.tools.hotswap.core.plugin.spring.scanner.ClassPathBeanRefreshCommand;
+import io.github.future0923.debug.tools.hotswap.core.plugin.solon.command.ClassPathBeanRefreshCommand;
 import io.github.future0923.debug.tools.hotswap.core.util.HaClassFileTransformer;
 import io.github.future0923.debug.tools.hotswap.core.util.signature.ClassChangesAnalyzer;
 
@@ -26,19 +25,17 @@ import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
 /**
- * SpringBean类文件Transformer，处理修改的class，即redefine类型的class
- *
  * @author future0923
  */
-public class SpringBeanClassFileTransformer implements HaClassFileTransformer {
+public class SolonBeanClassFileTransformer implements HaClassFileTransformer {
 
-    private static final Logger logger = Logger.getLogger(SpringBeanClassFileTransformer.class);
+    private static final Logger logger = Logger.getLogger(SolonBeanClassFileTransformer.class);
 
     private final ClassLoader appClassLoader;
     private final Scheduler scheduler;
     private final String basePackage;
 
-    public SpringBeanClassFileTransformer(ClassLoader appClassLoader, Scheduler scheduler, String basePackage) {
+    public SolonBeanClassFileTransformer(ClassLoader appClassLoader, Scheduler scheduler, String basePackage) {
         this.appClassLoader = appClassLoader;
         this.scheduler = scheduler;
         this.basePackage = basePackage;
@@ -50,10 +47,10 @@ public class SpringBeanClassFileTransformer implements HaClassFileTransformer {
             final ClassChangesAnalyzer analyzer = new ClassChangesAnalyzer(appClassLoader);
             className = className.replace("/", ".");
             if (analyzer.isReloadNeeded(classBeingRedefined, classfileBuffer)) {
-                logger.info("watch change class event, start reloading spring bean, class name:{}", className);
+                logger.info("watch change class event, start reloading solon bean, class name:{}", className);
                 scheduler.scheduleCommand(new ClassPathBeanRefreshCommand(classBeingRedefined.getClassLoader(), basePackage, className, classfileBuffer));
             } else {
-                logger.debug("watch change class event, There is no need to reload Spring beans, className:{}", className);
+                logger.debug("watch change class event, There is no need to reload solon beans, className:{}", className);
             }
         }
         return classfileBuffer;
