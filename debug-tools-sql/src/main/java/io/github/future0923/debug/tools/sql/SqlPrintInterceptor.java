abc975ee1939e09e68d93e4bc4b071f7d757bb15
Anwei <d7210c@163.com>
2025-06-12 23:45:58 +0800

    feat: remote host save & fix bugs

--- debug-tools-sql/src/main/java/io/github/future0923/debug/tools/sql/SqlPrintInterceptor.java
+++ debug-tools-sql/src/main/java/io/github/future0923/debug/tools/sql/SqlPrintInterceptor.java
@@ -135,6 +135,7 @@ public class SqlPrintInterceptor {
         if (PrintSqlType.PRETTY.equals(printSqlType) || PrintSqlType.YES.equals(printSqlType)) {
             resultSql = SqlFormatter.format(resultSql);
         }
+
         logger.info("Execute consume Time: {} ms; Execute SQL: \n\u001B[31m{}\u001B[0m", consume, resultSql);
     }
 }
