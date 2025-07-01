fc8facd532d52b3683eee4a354db6c32e6b4e12d
caoayu <1401262639@qq.com>
2025-07-01 11:33:18 +0800

    feat(toolbox): 添加设置按钮并更新相关功能
    - 在工具窗口添加设置按钮
    - 实现设置按钮点击事件处理逻辑
    - 新增 SettingAction 类用于设置操作- 更新 MainPanel 和 MainToolBar 类以支持设置功能
    - 添加设置图标并更新 DebugToolsIcons 类

---
+++ debug-tools-idea/src/main/java/io/github/future0923/debug/tools/idea/tool/action/SettingAction.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2024-2025 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.github.future0923.debug.tools.idea.tool.action;
+
+import com.intellij.openapi.options.ShowSettingsUtil;
+import com.intellij.openapi.project.Project;
+import io.github.future0923.debug.tools.idea.tool.DebugToolsToolWindow;
+import io.github.future0923.debug.tools.idea.utils.DebugToolsIcons;
+
+/**
+ * @author future0923
+ */
+public class SettingAction extends BaseToolAction {
+
+    public SettingAction() {
+        getTemplatePresentation().setText("Settings");
+        getTemplatePresentation().setIcon(DebugToolsIcons.Setting);
+    }
+
+    @Override
+    protected void doActionPerformed(Project project, DebugToolsToolWindow toolWindow) {
+        ShowSettingsUtil.getInstance().showSettingsDialog(project, "DebugToolsTool.Setting");
+    }
+} 
