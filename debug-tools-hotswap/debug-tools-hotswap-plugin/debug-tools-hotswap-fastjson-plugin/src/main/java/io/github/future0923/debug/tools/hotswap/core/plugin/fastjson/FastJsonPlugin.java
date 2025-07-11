cf8c6cd501f3cf4eb50c08e44a49b7f5461e5f14
future0923 <future94@qq.com>
2025-07-11 21:15:07 +0800

    fixed: fastjson2高版本没有getKotlinConstructor方法(#25)

--- debug-tools-hotswap/debug-tools-hotswap-plugin/debug-tools-hotswap-fastjson-plugin/src/main/java/io/github/future0923/debug/tools/hotswap/core/plugin/fastjson/FastJsonPlugin.java
+++ debug-tools-hotswap/debug-tools-hotswap-plugin/debug-tools-hotswap-fastjson-plugin/src/main/java/io/github/future0923/debug/tools/hotswap/core/plugin/fastjson/FastJsonPlugin.java
@@ -37,7 +37,7 @@ import java.util.concurrent.ConcurrentSkipListSet;
 @Plugin(
         name = "FastJson",
         description = "Reload FastJson cache after class definition/change.",
-        testedVersions = {"1.2.83", "2.0.6", "2.0.31"}
+        testedVersions = {"1.2.83", "2.0.6", "2.0.31", "2.0.51"}
 )
 public class FastJsonPlugin {
 
@@ -259,12 +259,16 @@ public class FastJsonPlugin {
             // fastjson 2+ 没有这个方法
         }
 
-        CtMethod getKotlinConstructor = ctClass.getDeclaredMethod("getKotlinConstructor", new CtClass[]{classCtClass, classPool.get("com.alibaba.fastjson2.codec.BeanInfo")});
-        getKotlinConstructor.insertBefore("{" +
-                "   if ($1 != null) {" +
-                "       constructorCache.remove($1);" +
-                "   }" +
-                "}");
+        try {
+            CtMethod getKotlinConstructor = ctClass.getDeclaredMethod("getKotlinConstructor", new CtClass[]{classCtClass, classPool.get("com.alibaba.fastjson2.codec.BeanInfo")});
+            getKotlinConstructor.insertBefore("{" +
+                    "   if ($1 != null) {" +
+                    "       constructorCache.remove($1);" +
+                    "   }" +
+                    "}");
+        } catch (NotFoundException e) {
+            // 高版本(如2.0.51)版本没有这个方法
+        }
 
         CtMethod constructor = ctClass.getDeclaredMethod("constructor", new CtClass[]{classCtClass, consumerCtClass});
         constructor.insertBefore("{" +
