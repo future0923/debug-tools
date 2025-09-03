aac8c6a1a76b9356fc01d62ed13fb561eb831a95
future0923 <future94@qq.com>
2025-09-03 13:31:15 +0800

    v4.3.1

--- debug-tools-base/src/main/java/io/github/future0923/debug/tools/base/constants/ProjectConstants.java
+++ debug-tools-base/src/main/java/io/github/future0923/debug/tools/base/constants/ProjectConstants.java
@@ -25,7 +25,7 @@ public interface ProjectConstants {
 
     String NAME = "DebugTools";
 
-    String VERSION = "4.3.0";
+    String VERSION = "4.3.1";
 
     String SPRING_EXTENSION_JAR_NAME = "debug-tools-extension-spring";
 
