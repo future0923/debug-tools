d8721d9118a549757892d5a45fa64bdba1c7b5ab
future0923 <future94@qq.com>
2025-09-26 15:26:54 +0800

    fixed(hotswap): 修复spring返回数据jackson偶尔热重载失败(#35)

--- debug-tools-hotswap/debug-tools-hotswap-plugin/debug-tools-hotswap-jackson-plugin/src/main/java/io/github/future0923/debug/tools/hotswap/core/plugin/jackson/JacksonPlugin.java
+++ debug-tools-hotswap/debug-tools-hotswap-plugin/debug-tools-hotswap-jackson-plugin/src/main/java/io/github/future0923/debug/tools/hotswap/core/plugin/jackson/JacksonPlugin.java
@@ -16,14 +16,24 @@
  */
 package io.github.future0923.debug.tools.hotswap.core.plugin.jackson;
 
+import io.github.future0923.debug.tools.base.logging.Logger;
 import io.github.future0923.debug.tools.hotswap.core.annotation.Init;
 import io.github.future0923.debug.tools.hotswap.core.annotation.LoadEvent;
 import io.github.future0923.debug.tools.hotswap.core.annotation.OnClassLoadEvent;
 import io.github.future0923.debug.tools.hotswap.core.annotation.Plugin;
+import io.github.future0923.debug.tools.hotswap.core.command.Command;
 import io.github.future0923.debug.tools.hotswap.core.command.Scheduler;
-import javassist.CannotCompileException;
-import javassist.NotFoundException;
 import io.github.future0923.debug.tools.hotswap.core.plugin.jackson.command.JacksonReloadCommand;
+import io.github.future0923.debug.tools.hotswap.core.util.PluginManagerInvoker;
+import javassist.CannotCompileException;
+import javassist.CtClass;
+import javassist.CtConstructor;
+import javassist.CtMethod;
+import javassist.CtNewMethod;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.WeakHashMap;
 
 /**
  * @author future0923
@@ -35,20 +45,88 @@ import io.github.future0923.debug.tools.hotswap.core.plugin.jackson.command.Jack
 )
 public class JacksonPlugin {
 
+    private static final Logger logger = Logger.getLogger(JacksonPlugin.class);
+
+    /**
+     * 生成清除的方法，热重载时候调用
+     */
+    public static final String CLEAR_CACHE_METHOD = "$$ha$clearCache";
+
+    /**
+     * jackson相关的对象
+     */
+    private final Set<Object> jacksonObj = Collections.synchronizedSet(Collections.newSetFromMap(new WeakHashMap<>()));
+
+    private final Command command = new JacksonReloadCommand(jacksonObj);
+
     @Init
-    static Scheduler scheduler;
+    public Scheduler scheduler;
 
-    private static boolean isJacksonEnv = false;
+    @OnClassLoadEvent(classNameRegexp = "com.fasterxml.jackson.databind.ObjectMapper", events = LoadEvent.DEFINE, skipSynthetic = false)
+    public static void patchObjectMapper(CtClass ctClass) throws CannotCompileException {
+        instantiateJackJson(ctClass);
+        CtMethod clearCacheMethod = CtNewMethod.make("public void " + CLEAR_CACHE_METHOD + "() {_rootDeserializers.clear();}", ctClass);
+        ctClass.addMethod(clearCacheMethod);
+        logger.info("patch jackson ObjectMapper success");
+    }
 
-    @OnClassLoadEvent(classNameRegexp = "com.fasterxml.jackson.databind.ObjectMapper")
-    public static void register(ClassLoader classLoader) throws NotFoundException, CannotCompileException {
-        isJacksonEnv = true;
+    @OnClassLoadEvent(classNameRegexp = "com.fasterxml.jackson.databind.ser.SerializerCache", events = LoadEvent.DEFINE, skipSynthetic = false)
+    public static void patchSerializerCache(CtClass ctClass) throws CannotCompileException {
+        instantiateJackJson(ctClass);
+        CtMethod clearCacheMethod = CtNewMethod.make("public void " + CLEAR_CACHE_METHOD + "() {flush();}", ctClass);
+        ctClass.addMethod(clearCacheMethod);
+        logger.info("patch jackson SerializerCache success");
+    }
+
+    @OnClassLoadEvent(classNameRegexp = "com.fasterxml.jackson.databind.deser.DeserializerCache", events = LoadEvent.DEFINE, skipSynthetic = false)
+    public static void patchDeserializerCache(CtClass ctClass) throws CannotCompileException {
+        instantiateJackJson(ctClass);
+        CtMethod clearCacheMethod = CtNewMethod.make("public void " + CLEAR_CACHE_METHOD + "() {flushCachedDeserializers();}", ctClass);
+        ctClass.addMethod(clearCacheMethod);
+        logger.info("patch jackson DeserializerCache success");
+    }
+
+    @OnClassLoadEvent(classNameRegexp = "com.fasterxml.jackson.databind.ser.impl.ReadOnlyClassToSerializerMap", events = LoadEvent.DEFINE, skipSynthetic = false)
+    public static void patchReadOnlyClassToSerializerMap(CtClass ctClass) throws CannotCompileException {
+        instantiateJackJson(ctClass);
+        CtMethod clearCacheMethod = CtNewMethod.make("public void " + CLEAR_CACHE_METHOD + "() {_buckets = new com.fasterxml.jackson.databind.ser.impl.ReadOnlyClassToSerializerMap.Bucket[_size];}", ctClass);
+        ctClass.addMethod(clearCacheMethod);
+        logger.info("patch jackson ReadOnlyClassToSerializerMap success");
+    }
+
+    @OnClassLoadEvent(classNameRegexp = "com.fasterxml.jackson.databind.type.TypeFactory", events = LoadEvent.DEFINE, skipSynthetic = false)
+    public static void patchTypeFactory(CtClass ctClass) throws CannotCompileException {
+        instantiateJackJson(ctClass);
+        CtMethod clearCacheMethod = CtNewMethod.make("public void " + CLEAR_CACHE_METHOD + "() { _typeCache.clear();}", ctClass);
+        ctClass.addMethod(clearCacheMethod);
+        logger.info("patch jackson TypeFactory success");
+    }
+
+    @OnClassLoadEvent(classNameRegexp = "com.fasterxml.jackson.databind.util.LRUMap", events = LoadEvent.DEFINE, skipSynthetic = false)
+    public static void patchLRUMap(CtClass ctClass) throws CannotCompileException {
+        instantiateJackJson(ctClass);
+        CtMethod clearCacheMethod = CtNewMethod.make("public void " + CLEAR_CACHE_METHOD + "() { clear();}", ctClass);
+        ctClass.addMethod(clearCacheMethod);
+        logger.info("patch jackson LRUMap success");
     }
 
     @OnClassLoadEvent(classNameRegexp = ".*", events = LoadEvent.REDEFINE)
-    public static void redefineClass(final Class<?> clazz, final ClassLoader appClassLoader) {
-        if (isJacksonEnv) {
-            scheduler.scheduleCommand(new JacksonReloadCommand(clazz, appClassLoader), 1000);
+    public void redefineClass() {
+        scheduler.scheduleCommand(command, 500);
+    }
+
+    private static void instantiateJackJson(CtClass ctClass) throws CannotCompileException {
+        StringBuilder src = new StringBuilder("{");
+        src.append(PluginManagerInvoker.buildInitializePlugin(JacksonPlugin.class));
+        src.append(PluginManagerInvoker.buildCallPluginMethod(JacksonPlugin.class, "registerNeedToClearCacheObjects", "this", "java.lang.Object"));
+        src.append("}");
+        for (CtConstructor constructor : ctClass.getDeclaredConstructors()) {
+            constructor.insertAfter(src.toString());
         }
     }
+
+    public void registerJacksonObj(Object objectMapper) {
+        jacksonObj.add(objectMapper);
+    }
+
 }
--- debug-tools-hotswap/debug-tools-hotswap-plugin/debug-tools-hotswap-jackson-plugin/src/main/java/io/github/future0923/debug/tools/hotswap/core/plugin/jackson/command/JacksonReloadCommand.java
+++ debug-tools-hotswap/debug-tools-hotswap-plugin/debug-tools-hotswap-jackson-plugin/src/main/java/io/github/future0923/debug/tools/hotswap/core/plugin/jackson/command/JacksonReloadCommand.java
@@ -16,86 +16,41 @@
  */
 package io.github.future0923.debug.tools.hotswap.core.plugin.jackson.command;
 
-import io.github.future0923.debug.tools.base.hutool.core.util.ReflectUtil;
 import io.github.future0923.debug.tools.base.logging.Logger;
 import io.github.future0923.debug.tools.hotswap.core.command.MergeableCommand;
 import io.github.future0923.debug.tools.hotswap.core.util.ReflectionHelper;
-import io.github.future0923.debug.tools.vm.JvmToolsUtils;
 
-import java.util.Map;
+import java.util.Collections;
+import java.util.Set;
+import java.util.WeakHashMap;
+
+import static io.github.future0923.debug.tools.hotswap.core.plugin.jackson.JacksonPlugin.CLEAR_CACHE_METHOD;
 
 /**
  * @author future0923
  */
-@SuppressWarnings({"unchecked", "rawtypes"})
 public class JacksonReloadCommand extends MergeableCommand {
 
     private static final Logger logger = Logger.getLogger(JacksonReloadCommand.class);
 
-    private final Class<?> clazz;
+    private final Set<Object> jacksonObj;
 
-    private final ClassLoader appClassLoader;
-
-    public JacksonReloadCommand(Class<?> clazz, ClassLoader appClassLoader) {
-        this.clazz = clazz;
-        this.appClassLoader = appClassLoader;
+    public JacksonReloadCommand(Set<Object> jacksonObj) {
+        this.jacksonObj = jacksonObj;
     }
 
     @Override
     public void executeCommand() {
-        Class<?> objectMapperClass;
-        try {
-            objectMapperClass = appClassLoader.loadClass("com.fasterxml.jackson.databind.ObjectMapper");
-        } catch (ClassNotFoundException e) {
-            return;
+        Set<Object> copy = Collections.newSetFromMap(new WeakHashMap<>());
+        synchronized (jacksonObj) {
+            copy.addAll(jacksonObj);
         }
-        Class deserializerCacheClass;
-        try {
-            deserializerCacheClass = appClassLoader.loadClass("com.fasterxml.jackson.databind.deser.DeserializerCache");
-        } catch (ClassNotFoundException e) {
-            return;
-        }
-        Class serializerCacheClass;
-        try {
-            serializerCacheClass = appClassLoader.loadClass("com.fasterxml.jackson.databind.ser.SerializerCache");
-        } catch (ClassNotFoundException e) {
-            return;
-        }
-        Object[] objectMappers = JvmToolsUtils.getInstances(objectMapperClass);
-        if (objectMappers == null || objectMappers.length == 0) {
-            return;
-        }
-        Object javaType;
-        try {
-            Object ob = objectMapperClass.getConstructor().newInstance();
-            javaType = ReflectUtil.invoke(ob, "constructType", clazz);
-        } catch (Exception e) {
-            javaType = ReflectUtil.invoke(objectMappers[0], "constructType", clazz);
-        }
-        boolean changed = false;
-        for (Object objectMapper : objectMappers) {
-            Map _rootDeserializers = (Map) ReflectionHelper.getNoException(objectMapper, objectMapperClass, "_rootDeserializers");
-            if (_rootDeserializers != null) {
-                boolean remove = _rootDeserializers.remove(javaType) != null;
-                if (!changed && remove) {
-                    changed = true;
-                }
+        for (Object obj : copy) {
+            try {
+                ReflectionHelper.invoke(obj, CLEAR_CACHE_METHOD);
+            } catch (Exception e) {
+                logger.error("Failed to clear Jackson cache", e);
             }
-        }
-        Object[] deserializerCaches = JvmToolsUtils.getInstances(deserializerCacheClass);
-        for (Object deserializerCache : deserializerCaches) {
-            ReflectUtil.invoke(deserializerCache, "flushCachedDeserializers");
-            Map _incompleteDeserializers = (Map) ReflectionHelper.getNoException(deserializerCache, deserializerCacheClass, "_incompleteDeserializers");
-            if (_incompleteDeserializers != null) {
-                _incompleteDeserializers.remove(javaType);
-            }
-        }
-        Object[] serializerCaches = JvmToolsUtils.getInstances(serializerCacheClass);
-        for (Object serializerCache : serializerCaches) {
-            ReflectUtil.invoke(serializerCache, "flush");
-        }
-        if (changed) {
-            logger.reload("Class '{}' has been reloaded.", clazz.getName());
         }
     }
 }
