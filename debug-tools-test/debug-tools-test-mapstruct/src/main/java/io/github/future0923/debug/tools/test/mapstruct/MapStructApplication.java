e93ac1aa70e5142a09e2d9ca2318fc553c66b55a
happyjustyn <35069723+happyjustyn@users.noreply.github.com>
2026-02-28 10:54:17 +0800

    test: 新增mapstruct测试用例 (#247)

---
+++ debug-tools-test/debug-tools-test-mapstruct/src/main/java/io/github/future0923/debug/tools/test/mapstruct/MapStructApplication.java
@@ -0,0 +1,11 @@
+package io.github.future0923.debug.tools.test.mapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapStructApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MapStructApplication.class, args);
+    }
+}
